cc90ff40c3d53094b4f812b8804db0d6833bd498
sowunmi mayowa <smayowa176@gmail.com>
2026-02-25 15:17:46 +0100

    updated navigation

---
+++ client/src/routes/Finance.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Finance = () => {
+  return (
+    <div className="mx-8 md:mx-12 xl:mx-auto xl:max-w-6xl">Finance page</div>
+  );
+};
+
+export default Finance;
---
+++ client/src/routes/Services.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div className="mx-8 md:mx-12 xl:mx-auto xl:max-w-6xl">Services Page</div>
+  );
+};
+
+export default Services;
